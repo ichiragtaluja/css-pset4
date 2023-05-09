de2332bb2a31762dbf8abccf73aeab07d47614ba
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-05-10 02:18:03 +0530

    add q4

--- src/App.js
+++ src/App.js
@@ -8,13 +8,42 @@ function PricingTable() {
   return (
     <div className="container">
       <div>
-        <p>One month free</p><h2>Basic Plan</h2>
+        <p>One month free</p>
+        <h2>Basic Plan</h2>
+        <p>$9.99/mo</p>
+        <hr />
+        <ul>
+          <li>Ad free music listening</li>
+          <li>Download 10k songs/device</li>
+          <li>Login from 3 devices</li>
+        </ul>
+        <button>Sign Up</button>
       </div>
       <div>
-        <p>One month free</p><h2>Pro Plan</h2>
+        <p>One month free</p>
+        <h2>Pro Plan</h2>
+        <p>$19.99/mo</p>
+        <hr />
+        <ul>
+          <li>Ad free music listening</li>
+          <li>Download 10k songs/device</li>
+          <li>Login from 5 devices</li>
+        </ul>
+        <button>Sign Up</button>
       </div>
       <div>
-        <p>One month free</p><h2>Enterprise Plan</h2>
+        <p>One month free</p>
+        <h2>Enterprise Plan</h2>
+        <p>$59.99/mo</p>
+        <hr />
+        <div>
+          <ul>
+            <li>Ad free music listening</li>
+            <li>Download 10k songs/device</li>
+            <li>Login from unlimited devices</li>
+          </ul>
+          <button>Sign Up</button>
+        </div>
       </div>
     </div>
   );
